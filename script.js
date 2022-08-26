f8f4968d71d443cb37df73ff58f2eccb6c7d61e0
TexasMadeKing <JoshBennetsen@gmail.com>
2022-08-26 15:25:03 -0500

    Update Again

--- script.js
+++ script.js
@@ -25,11 +25,11 @@ let weather = {
     document.querySelector(".icon").src =
       "https://openweathermap.org/img/wn/" + icon + ".png";
     document.querySelector(".description").innerText = description;
-    document.querySelector(".temp").innerText = temp + "°C";
+    document.querySelector(".temp").innerText = temp + "°F";
     document.querySelector(".humidity").innerText =
       "Humidity: " + humidity + "%";
     document.querySelector(".wind").innerText =
-      "Wind speed: " + speed + " km/h";
+      "Wind speed: " + speed + " mph";
     document.querySelector(".weather").classList.remove("loading");
     document.body.style.backgroundImage =
       "url('https://source.unsplash.com/1600x900/?" + name + "')";
